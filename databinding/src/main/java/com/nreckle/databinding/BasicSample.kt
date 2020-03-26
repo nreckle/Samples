469af8df4dafbdfba35940bf655e3b7608dfeff6
zhaopengchao <zhaopengchao-os@360os.com>
2020-03-26 11:30:39 +0800

    Fix instantiate viewModel

--- databinding/src/main/java/com/nreckle/databinding/BasicSample.kt
+++ databinding/src/main/java/com/nreckle/databinding/BasicSample.kt
@@ -3,8 +3,9 @@ package com.nreckle.databinding
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.nreckle.databinding.databinding.ActivityBasicSampleBinding
+import androidx.lifecycle.ViewModelProvider
 import com.nreckle.databinding.data.BasicSampleViewModel
+import com.nreckle.databinding.databinding.ActivityBasicSampleBinding
 
 class BasicSample : AppCompatActivity() {
 
@@ -13,7 +14,7 @@ class BasicSample : AppCompatActivity() {
 
         val binding: ActivityBasicSampleBinding = DataBindingUtil.setContentView(
                 this, R.layout.activity_basic_sample)
-        val myViewModel = BasicSampleViewModel()
+        val myViewModel = ViewModelProvider(this).get(BasicSampleViewModel::class.java);
         binding.viewmodel = myViewModel
         binding.lifecycleOwner = this
     }
